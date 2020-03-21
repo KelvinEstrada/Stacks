c0e10e10df711d13a967a88a621ad4d7ae65984e
kelvin <kelvin@kelvin-MS-7A75>
2020-03-21 16:48:14 -0400

    Must fix operations.

--- Lab3/src/ciic4020/stack/test/Fully_Paren.java
+++ Lab3/src/ciic4020/stack/test/Fully_Paren.java
@@ -5,83 +5,47 @@ import ciic4020.stack.Stack;
 
 public class Fully_Paren {
 
+	public static Integer[] VARIABLEVALUES = new Integer[26];
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Stack<Integer> operands = new LinkedListStack<Integer>();
-		Stack<Character> operators = new LinkedListStack<Character>();
-		Stack<Character> parenthesis = new LinkedListStack<Character>();
-		Stack<Integer> results = new LinkedListStack<Integer>();
+		System.out.println("Enter an expression: ");
 		while(true) {
-			System.out.println("Enter an expression: ");
 			String input = scanner.nextLine();
 			if(input.isEmpty()) {
-				System.out.println("No expression typed. Closing...");
 				break;
+			}
+			if(Character.isAlphabetic(input.charAt(0)) && input.charAt(1) == '=') {
+				VARIABLEVALUES[((int)input.charAt(0)) - 65] = Integer.parseInt(input.substring(2));
+				System.out.println(VARIABLEVALUES[((int)input.charAt(0)) - 65]);
 			}
 			else if(!isBalanced(input)) {
-				System.out.println(input + " " + "is invalid.");
-				break;
+				System.out.println(input + " " + "is invalid");
 			}
-			for(int i = 0; i < input.length(); i++) {
-				if(Character.isDigit(input.charAt(i))) {
-					String number = "";
-					while(Character.isDigit(input.charAt(i))) {
-						number += input.charAt(i);
-						i++;
-					}
-					operands.push(Integer.parseInt(number));
-					number = "";
-				}
-				else if(input.charAt(i) == '+') {
-					operators.push(input.charAt(i));
-				}
-				else if(input.charAt(i) == '-') {
-					operators.push(input.charAt(i));
-				}
-				else if(input.charAt(i) == '*') {
-					operators.push(input.charAt(i));
-				}
-				else if(input.charAt(i) == '/') {
-					operators.push(input.charAt(i));
-				}
-				else if(input.charAt(i) == '(' || input.charAt(i) == '[' || input.charAt(i) == '{') {
-					parenthesis.push(input.charAt(i));
-				}
-			}
-			while(parenthesis.top() == '(' || parenthesis.top() == '[' || parenthesis.top() == '{') {
-				results.push(operands.pop());
-				results.push(operands.pop());
-				Character op = operators.pop();
-				if(op == '+') {
-					int result = results.pop() + results.pop();
-					results.push(result);
-					parenthesis.pop();
-				}
-				if(op == '-') {
-					int result = results.pop() - results.pop();
-					results.push(result);
-					parenthesis.pop();
-				}
-				if(op == '*') {
-					int result = results.pop() * results.pop();
-					results.push(result);
-					parenthesis.pop();
-				}
-				if(op == '/') {
-					int result = results.pop() / results.pop();
-					results.push(result);
-					parenthesis.pop();
-				}
+			else {
+				evaluate(input);
 			}
 		}
-		printStack(operands);
+		for(int i = 0; i < VARIABLEVALUES.length; i++) {
+			if(VARIABLEVALUES[i] != null) {
+				Character value = (char) (i+65);
+				System.out.println(value + "\t" + VARIABLEVALUES[i]);
+			}
+		}
 	}
-	private static void printStack(Stack<Integer> parenthesis) {
-		Object[] s = parenthesis.toArray();
+
+	/*	Print method for testing purposes
+	 * 
+	 */
+	private static void printStack(Stack<Integer> stack) {
+		Object[] s = stack.toArray();
 		for(int i = 0; i < s.length; i++) {
 			System.out.println(s[i]);
 		}
 	}
+	/*	Verifies if expression has balanced parenthesis
+	 * 
+	 */
 	public static boolean isBalanced(String input) {
 		Stack<Character> stack = new LinkedListStack<Character>();
 		for(int i = 0; i < input.length(); i++) {
@@ -104,6 +68,66 @@ public class Fully_Paren {
 			}
 		}
 		return stack.isEmpty();
+	}
+	/*	After expression is verified to be balanced, this methods evaluates the expression
+	 * 
+	 */
+	public static Integer[] evaluate(String in) {
+		Stack<Integer> operands = new LinkedListStack<Integer>();
+		Stack<Character> operators = new LinkedListStack<Character>();
+		Stack<Character> parenthesis = new LinkedListStack<Character>();
+		Stack<Integer> results = new LinkedListStack<Integer>();
+
+		for(int i = 0; i < in.length(); i++) {
+			if(in.charAt(i) == '(' || in.charAt(i) == '['|| in.charAt(i) == '{') {
+				parenthesis.push(in.charAt(i));
+			}
+			else if(in.charAt(i) == '+' || in.charAt(i) == '-' || in.charAt(i) == '*' || in.charAt(i) == '/') {
+				operators.push(in.charAt(i));
+			}
+			else if(Character.isLetter(in.charAt(i)) && i != 0) {
+				int val = VARIABLEVALUES[(int)in.charAt(i) - 65];
+				operands.push(val);
+			}
+		}
+		for(int i = 0; i < in.length(); i++) {
+			if(in.charAt(i) == ')' || in.charAt(i) == ']' || in.charAt(i) == '}') {
+				if(operands.size() >= 2 || !operators.isEmpty()){
+					int operand1 = operands.pop();
+					int operand2 = operands.pop();
+					Character operator = operators.pop();
+					if(operator == '+') {
+						int res = operand1 + operand2;
+						results.push(res);
+						operands.push(res);
+						/*	Since the condition of operation is satisfied, the expression is valid
+						 * 	and we can assume that the character at position 0 will hold the value of the expression.
+						 * 	Then we change its value in the VARIABLES VALUES array.
+						 */
+						VARIABLEVALUES[(int)in.charAt(0) - 65] = results.pop();
+					}
+					if(operator == '-') {
+						int res = operand2 - operand1;
+						results.push(res);
+						operands.push(res);
+						VARIABLEVALUES[(int)in.charAt(0) - 65] = results.pop();
+					}
+					if(operator == '*') {
+						int res = operand1 * operand2;
+						results.push(res);
+						operands.push(res);
+						VARIABLEVALUES[(int)in.charAt(0) - 65] = results.pop();
+					}
+					if(operator == '/') {
+						int res = operand1 / operand2;
+						results.push(res);
+						operands.push(res);
+						VARIABLEVALUES[(int)in.charAt(0) - 65] = results.pop();
+					}
+				}
+			}
+		}
+		return VARIABLEVALUES;
 	}
 }
 
